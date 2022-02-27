c0b1b0c517fd355e57da50c5e710d446b1a640a3
Carissa <carissa.cks@gmail.com>
2022-02-27 19:44:08 +0700

    initial commit

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,40 +1,80 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    entry: path.resolve(__dirname, '..', './src/index.tsx'), // our entry point, as mentioned earlier
-    mode: 'development',
-    module: {
-        rules: [
-            {
-                test: /\.[jt]sx?$/, // matches .js, .ts, and .tsx files
-                use: ['babel-loader'],
-                exclude: /node_modules/,
+  entry: path.resolve(__dirname, '../src/index.tsx'), // our entry point, as mentioned earlier
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.[jt]sx?$/, // matches .js, .ts, and .tsx files
+        use: ['babel-loader'],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(css|less)$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader', // translates CSS into CommonJS
+          },
+          {
+            loader: 'less-loader', // compiles Less to CSS
+            options: {
+              lessOptions: {
+                modifyVars: {
+                  hack: `true; @import "${path.resolve(
+                    __dirname,
+                    '../src/theme.less',
+                  )}";`,
+                },
+                javascriptEnabled: true,
+              },
             },
-            {
-                test: /\.css$/, // matches .css files only (i.e. not .scss, etc)
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-                type: 'asset/resource',
-            },
-            {
-                test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
-                type: 'asset/inline',
-            },
+          },
         ],
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js', '.jsx'],
-    },
-    output: {
-        path: path.resolve(__dirname, '..', './build'),
-        filename: 'bundle.js', // our output bundle
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, '..', './src/index.html'),
-        }),
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|)$/,
+        type: 'asset/inline',
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+          {
+            loader: '@svgr/webpack',
+            options: {
+              babel: false,
+              icon: true,
+            },
+          },
+        ],
+      },
     ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '../src/index.html'),
+      favicon: path.resolve(__dirname, '../assets/fav-icon.png'),
+    }),
+    new CleanWebpackPlugin(),
+  ],
 };
--- webpack/webpack.development.js
+++ webpack/webpack.development.js
@@ -2,14 +2,15 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-    mode: 'development',
-    devServer: {
-        contentBase: path.join(__dirname, '..', './src'),
-        port: 3000,
-        open: true,
-        compress: true,
-        hotOnly: true,
-    },
-    devtool: 'cheap-module-source-map',
-    plugins: [new webpack.HotModuleReplacementPlugin()],
+  mode: 'development',
+  devServer: {
+    contentBase: path.join(__dirname, '../src'),
+    port: 3000,
+    open: true,
+    compress: true,
+    hotOnly: true,
+    historyApiFallback: true,
+  },
+  devtool: 'cheap-module-source-map',
+  plugins: [new webpack.HotModuleReplacementPlugin()],
 };
